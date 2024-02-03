aaf882eb3e9171eb4b5c15d6d03087eb7051670a
DEN <zmiienko.denys@gmail.com>
2024-02-03 12:10:50 +0200

    15

--- DZ_Python_15.py
+++ DZ_Python_15.py
@@ -3,7 +3,7 @@
 # полях класса: название книги, год выпуска, издателя,
 # жанр, автора, цену. Реализуйте методы класса для ввода
 # данных (сеттеры), вывода данных (геттеры), создайте конструктор с параметрами.
-
+#
 class Book:
     def __init__(self, title_book,year_publication,publisher,genre,author,price):
 
@@ -118,148 +118,77 @@ print(f"Division = {f6}")
 # Опыт (experience)
 # Реализуйте конструктор класса, который инициализирует атрибуты персонажа с некоторыми начальными значениями.
 # Добавьте метод для атаки, который будет учитывать силу атаки и защиту персонажа.
-# Реализуйте метод для получения опыта. При достижении определенного количества опыта персонаж должен поднимать уровень, сопровождаемый увеличением характеристик.
-# Создайте несколько экземпляров класса "GameCharacter" с разными характеристиками и протестируйте их, проводя атаки и получая опыт.
+# Реализуйте метод для получения опыта. При достижении определенного количества опыта персонаж должен поднимать уровень,
+# сопровождаемый увеличением характеристик. Создайте несколько экземпляров класса "GameCharacter" с разными
+# характеристиками и протестируйте их, проводя атаки и получая опыт.
 
-# class Unit:
-#     def __init__(self, name, clan):
-#         self.name = name
-#         self.clan = clan
-#         self.max_health = 100
-#         self.min_health = 0
-#         self.max_strength = 10
-#         self.min_strength = 0
-#         self.max_agility = 10
-#         self.min_agility = 0
-#         self.max_intelligence = 10
-#         self.min_intelligence = 0
-#
-#     def read_unit(self):
-#         read = self.name + " " + self.clan
-#         return read.title()
-#
-#     def read_health(self):
-#         if self.min_health + 10 > self.max_health:
-#             self.max_health = 100
-#         else:
-#             self.min_intelligence += 10
-#
-#     def health_read(self):
-#         print("У персонажа: " + str.read_health + " hp")
-#
-#
-# class Mage(Unit):
-#     def __init__(self, name, clan, air, fire, water):
-#         super().__init__(name, clan, air, fire, water)
-#
-#     def intelligence_mage(self):
-#         if self.min_intelligence + 1 < self.max_intelligence:
-#             self.max_intelligence = 10
-#         else:
-#             self.min_intelligence += 10
-#
-#
-# class Archer(Unit):
-#     def __init__(self, name, clan, bow, crossbow):
-#         super().__init__(name, clan, bow, crossbow)
-#
-#     def agility_archer(self):
-#         if self.min_agility + 1 < self.max_agility:
-#             self.max_agility = 10
-#         else:
-#             self.min_agility += 10
-#
-#
-# class Knight(Unit):
-#     def __init__(self, name, clan, sword, axe, pike):
-#         super().__init__(self, name, clan, sword, axe, pike)
-#
-#     def agility_knight(self):
-#         if self.min_strengthx + 1 < self.max_strength:
-#             self.max_strength = 10
-#         else:
-#             self.min_strength += 10
-#
-#
-# Eridan = Mage("Eridan", "Fresh", "fire")
-# print(Eridan.read_unit())
-# print(Eridan.intelligence_mage)
-# Eridan.health_read()
-#
-# Erifan = Mage("Erifan", "Dradon", "water")
-# print(Erifan.read_unit())
-# print(Erifan.intelligence_mage)
-# Erifan.health_read()
-#
-# Eren = Knight("Eren", "Fresh", "pike")
-# print(Eren.read_unit())
-# print(Eren.agility_knight)
-# Eren.health_read()
-#
-# Selfi = Archer("Selfi", "Archer_best", "crossbow")
-# print(Selfi.read_unit())
-# print(Selfi.agility_archer)
-# Selfi.health_read()
-#
-# Antalia = Mage("Antalia", "Fresh", "air")
-# print(Antalia.read_unit())
-# print(Antalia.intelligence_mage)
-# Antalia.health_read()
-#
-# Enifar = Archer("Enifar", "Archer_best", "bow")
-# print(Enifar.read_unit())
-# print(Enifar.agility_archer)
-# Enifar.health_read()
-#
-#
-# class Unit:
-#     def __init__(self, name, clan):
-#         self.name = name
-#         self.clan = clan
-#         self.health = 100
-#         self.power = 10
-#         self.agility = 10
-#         self.intellect = 10
-#
-#     def therapy(self):
-#         if self.health < 100:
-#             self.health += 10
-#
-#
-# class Mage(Unit):
-#     def __init__(self, name, clan, air, fire, water):
-#         super().__init__(name, clan)
-#         self.air = air
-#         water
-#         self.fire = fire
-#         self.water = water
-#
-#     def augment(self):
-#         if self.intellect < 10:
-#             self.intellect += 1
-#
-#
-# class Archer(Unit):
-#     def __init__(self, name, clan, bow, crossbow):
-#         super().__init__(name, clan)
-#         self.bow = bow
-#         self.crossbow = crossbow
-#
-#     def augment(self):
-#         if self.agility < 10:
-#             self.agility += 1
-#
-#
-# class Knight(Unit):
-#     def __init__(self, name, clan, sword, axe, pike):
-#         super().__init__(self, name)
-#         self.sword = sword
-#         self.axe = axe
-#         self.pike = pike
-#
-#     def augment(self):
-#         if self.power < 10:
-#             self.power += 1
-#
-#
-# 0
+class GameCharacter:
+    def __init__(self, name, level, health, attack_power, defense, experience):
+        self.name = name
+        self.level = level
+        self.health = health
+        self.attack_power = attack_power
+        self.defense = defense
+        self.experience = experience
+
+    def __del__(self):
+        print()
+
+    def showGameCharacter(self):
+        return (f"Unit name: {self.name} , Unit level: {self.level} , Unit health: {self.health} ,"
+                f"Unit attack power: {self.attack_power} , Unit defense: {self.defense} , Unit experience: {self.experience}")
+
+    def get_name(self):
+        return self.name
+    def set_name(self, new_name):
+        self.name = new_name
+
+    def get_level(self):
+        return self.level
+    def set_level(self, new_level):
+        self.level = new_level
+
+    def get_health(self):
+        return self.health
+    def set_health(self, new_health):
+        self.health = new_health
+
+    def get_attack_power(self):
+        return self.attack_power
+    def set_attack_power(self, new_attack_power):
+        self.attack_power = new_attack_power
+
+    def get_defense(self):
+        return self.defense
+    def set_defense(self, new_defense):
+        self.defense = new_defense
+
+    def get_experience(self):
+        return self.experience
+    def set_experience(self, new_experience):
+        self.experience = new_experience
+
+    def augment_attack_power(self):
+        if self.attack_power < 100:
+            self.attack_power += 1
+
+    def augment_defense(self):
+        if self.defense < 100:
+            self.defense += 1
+
+    def augment_level(self):
+        if self.experience > 20:
+            self.level += 1
+
+    def therapy(self):
+        if self.health < 100:
+            self.health += 10
+
+Unit1 = GameCharacter("Captain America", 5, 57, 80, 60, 67 )
+Unit2 = GameCharacter("Iron Man", 6, 60, 76, 65, 66)
+Unit3 = GameCharacter("Spider man", 4, 70, 55, 45, 41)
+
+print(Unit1.showGameCharacter())
+print(Unit2.showGameCharacter())
+print(Unit3.showGameCharacter())
+
